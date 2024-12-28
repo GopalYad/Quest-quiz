5705d6ae4e8c19bbe661fce16ed800c4d6f1a1d3
Gopal yadav <yadavgopal6675@gmail.com>
2024-12-29 00:11:17 +0530

    delete

--- js/script.js
+++
@@ -1,3 +0,0 @@
-import { switchMode } from "./switchtogglebtn.js";
-const toggleSwitch = document.querySelector('.light-dark-switch input[type ="checkbox"]')
-toggleSwitch.addEventListener('change', switchMode, false);
