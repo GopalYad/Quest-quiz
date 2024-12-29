354abf624528cec095f2aa0f8b9bd3fc1ae29863
Gopal yadav <yadavgopal6675@gmail.com>
2024-12-29 18:01:43 +0530

    fetch data.json and create container for questions

--- js/App.js
+++ js/App.js
@@ -8,3 +8,26 @@ actionToggle.addEventListener('click', (event) => {
         document.documentElement.setAttribute('data-theme', 'light');
     }
 })
+const response = await fetch('./data.json');
+const data = await response.json();
+
+
+console.log(data);
+data.quizzes.forEach((item) => {
+    console.log(item.title); 
+});
+
+
+data.quizzes.forEach((item)=>{
+    console.log(item.icon)
+})
+
+data.quizzes.forEach((item)=>{
+    item.questions.forEach((item)=>{
+      console.log(item.options)
+    })
+})
+
+
+
+
